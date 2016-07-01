a2870c37f9c9d499e2e20d5837a01435976cd7f1
LeKieuPhuongAnh <polee3459@gmail.com>
2016-07-01 16:54:39 +0700

    Cập nhật giao diện

--- DOANCUOIKY/DOANCUOIKY/GUI/frmLogin.Designer.cs
+++ DOANCUOIKY/DOANCUOIKY/GUI/frmLogin.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace DOANCUOIKY.GUI
+namespace DOANCUOIKY.GUI
 {
     partial class frmLogin
     {
@@ -34,77 +34,102 @@
             this.label2 = new System.Windows.Forms.Label();
             this.edtUsername = new System.Windows.Forms.TextBox();
             this.edtPassword = new System.Windows.Forms.TextBox();
-            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(38, 211);
+            this.btnLogin.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLogin.Location = new System.Drawing.Point(84, 279);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Size = new System.Drawing.Size(358, 23);
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "Đăng nhập";
-            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(179, 211);
+            this.btnExit.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnExit.Location = new System.Drawing.Point(84, 344);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(358, 23);
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "Thoát";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 89);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(12, 186);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(71, 16);
             this.label1.TabIndex = 4;
             this.label1.Text = "Username";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 115);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(12, 224);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(68, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Password";
             // 
             // edtUsername
             // 
-            this.edtUsername.Location = new System.Drawing.Point(94, 89);
+            this.edtUsername.Location = new System.Drawing.Point(115, 182);
             this.edtUsername.Name = "edtUsername";
-            this.edtUsername.Size = new System.Drawing.Size(146, 20);
+            this.edtUsername.Size = new System.Drawing.Size(279, 20);
             this.edtUsername.TabIndex = 6;
+            this.edtUsername.TextChanged += new System.EventHandler(this.edtUsername_TextChanged);
             // 
             // edtPassword
             // 
-            this.edtPassword.Location = new System.Drawing.Point(94, 112);
+            this.edtPassword.Location = new System.Drawing.Point(115, 223);
             this.edtPassword.Name = "edtPassword";
-            this.edtPassword.Size = new System.Drawing.Size(146, 20);
+            this.edtPassword.Size = new System.Drawing.Size(279, 20);
             this.edtPassword.TabIndex = 7;
             // 
-            // btnCreateAccount
+            // label3
             // 
-            this.btnCreateAccount.Location = new System.Drawing.Point(179, 12);
-            this.btnCreateAccount.Name = "btnCreateAccount";
-            this.btnCreateAccount.Size = new System.Drawing.Size(101, 23);
-            this.btnCreateAccount.TabIndex = 8;
-            this.btnCreateAccount.Text = "tao tai khoan";
-            this.btnCreateAccount.UseVisualStyleBackColor = true;
-            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(151, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ĐĂNG NHẬP";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DOANCUOIKY.Properties.Resources.mzi_quwynfpc_175x175_75;
+            this.pictureBox1.Location = new System.Drawing.Point(372, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 150);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.btnCreateAccount);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(542, 394);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.edtPassword);
             this.Controls.Add(this.edtUsername);
             this.Controls.Add(this.label2);
@@ -113,6 +138,7 @@
             this.Controls.Add(this.btnLogin);
             this.Name = "frmLogin";
             this.Text = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,6 +152,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox edtUsername;
         private System.Windows.Forms.TextBox edtPassword;
-        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
